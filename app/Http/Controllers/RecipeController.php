d41d75f347fe74245bf17e2ac9ffed2811619f61
Oyebanji Oluwatomisin <oluwatomisinoyebanji@gmail.com>
2025-02-01 00:39:49 +0100

    first commit

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    /**
+     * Summary of index
+     * Get a list of all recipes
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Summary of store
+     * Store a new recipe
+     * @param \Illuminate\Http\Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Summary of show
+     * get a recipe by id
+     * @param string $id
+     * @return void
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Summary of update
+     * modify a recipe.
+     * @param \Illuminate\Http\Request $request
+     * @param string $id
+     * @return void
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Summary of destroy
+     * delete a recipe.
+     * @param string $id
+     * @return void
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
